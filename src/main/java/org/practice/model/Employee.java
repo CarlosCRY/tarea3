a9503e80b304ede7a2731ae726eae278766929e7
CarlosCRY <trueccr@gmail.com>
2025-12-01 12:41:20 +0100

    Casual monday?

---
+++ src/main/java/org/practice/model/Employee.java
@@ -0,0 +1,15 @@
+package org.practice.model;
+
+public class Employee {
+    private String nif;
+    private String name;
+    private String surname;
+
+    public Employee(String nif, String name, String surname) {
+        this.nif = nif;
+        this.name = name;
+        this.surname = surname;
+    }
+
+
+}
